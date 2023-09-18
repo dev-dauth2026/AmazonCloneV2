593afcf41395f8b0a28296ec41be79f1f1ecb629
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-19 02:31:36 +1000

    migration of model added

--- shop/admin.py
+++ shop/admin.py
@@ -1,22 +1,104 @@
 from django.contrib import admin
-from .models import User, Category, Product,ProductImage
+from .models import User, Category, Colour,ProductImage,Product,Accessories,ComputerAndPhone,Clothe,HomeLiving,BeautyPersonalCare,SportsOutdoors,ElectronicsBrand,ClotheBrand,HomeLivingBrand,BeautyPersonalCareBrand,SportsOutdoorsBrand
 
 # Register your models here.
+
+# User Model 
 class UserAdmin(admin.ModelAdmin):
   list_display = ("first_name", "last_name")
 
 admin.site.register(User,UserAdmin)
 
+# Product Model 
 class ProductAdmin(admin.ModelAdmin):
   list_display = ("name", "category", "slug","main_image","created_at",)
   prepopulated_fields = {"slug": ["name"]}
 
 admin.site.register(Product,ProductAdmin)
 
+# Category Model 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "description")
  
 admin.site.register(Category,CategoryAdmin)
 
+
+# Brand Model 
+
+class ElectronicsBrandAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(ElectronicsBrand,ElectronicsBrandAdmin)
+
+
+class ClotheBrandAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(ClotheBrand,ClotheBrandAdmin)
+
+
+class HomeLivingBrandAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(HomeLivingBrand,HomeLivingBrandAdmin)
+
+class BeautyPersonalCareBrandAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(BeautyPersonalCareBrand,BeautyPersonalCareBrandAdmin)
+
+
+class SportsOutdoorsBrandAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(SportsOutdoorsBrand,SportsOutdoorsBrandAdmin)
+# Colour Model 
+
+class ColourAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+ 
+admin.site.register(Colour,ColourAdmin)
+
+#  ProductImage Model 
+
 admin.site.register(ProductImage)
+
+# Electronics
+class AccessoriesAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(Accessories,AccessoriesAdmin)
+
+
+# ComputerAndPhone Model 
+class ComputerAndPhoneAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(ComputerAndPhone,ComputerAndPhoneAdmin)
+
+# Clothe Model 
+class ClotheAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(Clothe,ClotheAdmin)
+
+# HomeLiving Model 
+class HomeLivingAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(HomeLiving,HomeLivingAdmin)
+
+# BeautyPersonalCare Model 
+class BeautyPersonalCareAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(BeautyPersonalCare,BeautyPersonalCareAdmin)
+
+# SportsOutdoors Model 
+class SportsOutdoorsAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+ 
+admin.site.register(SportsOutdoors,SportsOutdoorsAdmin)
+
 
--- shop/models.py
+++ shop/models.py
@@ -18,6 +18,39 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class ElectronicsBrand(Brand):
+    pass
+    
+
+class ClotheBrand(Brand):
+    pass
+
+
+class HomeLivingBrand(Brand):
+    pass
+
+class BeautyPersonalCareBrand(Brand):
+    pass
+
+class SportsOutdoorsBrand(Brand):
+    pass
+
+class ToysGamesBrand(Brand):
+    pass
+class Colour(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 
 class ProductImage(models.Model):
     image = models.ImageField(upload_to='additional_images/')
@@ -33,6 +66,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    colour = models.ForeignKey(Colour, on_delete=models.CASCADE, null=True, blank=True)
     slug = models.SlugField(null=True,blank=True, unique=True)
     main_image = models.ImageField(upload_to='product_images/', null=True, blank=True)
     additional_images = models.ManyToManyField('ProductImage', related_name='products')
@@ -47,6 +81,86 @@ class Product(models.Model):
             self.slug=slugify(self.name)
         return super().save(*args, **kwargs)
     
+class Electronics(Product):
+    size_choices = (
+        ('small', 'Small'),
+        ('medium', 'Medium'),
+        ('large', 'Large'),
+    )
+    size = models.CharField(max_length=10, choices=size_choices,default='small')
+    subCategory_choices = (
+        ('computer', 'Computer'),
+        ('laptop', 'Laptop'),
+        ('phone', 'Phone'),
+        ('tablet', 'Tablet'),
+        ('accessories', 'accessories'),
+    )
+    subCategory = models.CharField(max_length=50, choices=subCategory_choices,default='small')
+    brand = models.ForeignKey(ElectronicsBrand, on_delete=models.CASCADE, null=True, blank=True)
+
+
+
+class ComputerAndPhone(Electronics):
+    screen_size = models.CharField(max_length=50)
+    processor_type = models.CharField(max_length=100)
+    storage_capacity = models.CharField(max_length=100)
+
+class Accessories(Electronics):
+    pass
+  
+
+class Clothe(Product):
+    brand = models.ForeignKey(ClotheBrand, on_delete=models.CASCADE, null=True, blank=True)
+    material = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    season = models.CharField(max_length=50)
+    size_choices = (
+        ('xs', 'Extra Small'),
+        ('s', 'Small'),
+        ('m', 'Medium'),
+        ('l', 'Large'),
+        ('xl', 'Extra Large'),
+    )
+    size = models.CharField(max_length=10, choices=size_choices,default='xs')
+    gender_choices = (
+        ('men', 'Men'),
+        ('women', 'Women'),
+        ('kids', 'Kids'),
+    )
+    gender = models.CharField(max_length=10, choices=gender_choices, default='men')
+
+
+class HomeLiving(Product):
+    brand = models.ForeignKey(HomeLivingBrand, on_delete=models.CASCADE, null=True, blank=True)
+    room_type = models.CharField(max_length=100)
+    material = models.CharField(max_length=100)
+    dimensions = models.CharField(max_length=100)
+
+
+class BeautyPersonalCare(Product):
+    brand = models.ForeignKey(BeautyPersonalCareBrand, on_delete=models.CASCADE, null=True, blank=True)
+    SKIN_TYPES = [
+        ('normal', 'Normal'),
+        ('dry', 'Dry'),
+        ('oily', 'Oily'),
+        ('combination', 'Combination'),
+        ('sensitive', 'Sensitive'),
+    ]
+
+    skin_type = models.CharField(max_length=20, choices=SKIN_TYPES, default='normal')
+    fragrance_type = models.CharField(max_length=100)
+
+
+class SportsOutdoors(Product):
+    brand = models.ForeignKey(SportsOutdoorsBrand, on_delete=models.CASCADE, null=True, blank=True)
+    sport_type = models.CharField(max_length=100)
+
+
+
+class ToysGames(Product):
+    brand = models.ForeignKey(SportsOutdoorsBrand, on_delete=models.CASCADE, null=True, blank=True)
+    age_group = models.CharField(max_length=50)
+    skill_level = models.CharField(max_length=50)
 
 
 class TrendingProduct(models.Model):
