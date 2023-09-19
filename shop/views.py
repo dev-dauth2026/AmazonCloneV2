ff4d5a47cf140a3b68acc2710f333546282a7f3e
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-20 00:04:48 +1000

    product detail functionality added

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse    
 from django.template import loader
 from .models import Product,Category
@@ -14,3 +14,10 @@ def shop(request):
     template= loader.get_template("main.html")
 
     return HttpResponse (template.render({'category_products':category_products},request))
+
+
+def productDetail(request,slug):
+    product = get_object_or_404(Product, slug=slug)
+    template=loader.get_template("products/productDetail.html")
+
+    return HttpResponse(template.render({'product':product},request))
