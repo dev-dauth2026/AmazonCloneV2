7abef71199da5b06fb5485f0f7b1a1ac49d2f0f9
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-29 00:06:45 +1000

    shop app url name added

--- amazonclone/urls.py
+++ amazonclone/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
-    path('',include('shop.urls')),
+    path('',include('shop.urls'),name="home"),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
