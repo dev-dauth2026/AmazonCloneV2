028b354f8ca92e1624bc45400703d195d2f15f94
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-29 00:02:57 +1000

    cart model removed

--- shop/models.py
+++ shop/models.py
@@ -1,15 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
 from django.template.defaultfilters import slugify
 
-
-# Create your models here.
-class User(AbstractUser):
-    pass
-
-    def __str__(self):
-        return self.name
-    
 
 class Category(models.Model):
     name=models.CharField(max_length=100, unique=True)
@@ -170,3 +162,14 @@ class TrendingProduct(models.Model):
 class MostSoldProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
+# class Cart(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+# class CartItem(models.Model):
+#     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField(default=1)
+
+#     def __str__(self):
+#         return f"{self.product.name} - {self.quantity}"
