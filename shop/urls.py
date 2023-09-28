9f5d4cdd8181ffe4825db4d37f1c9c4921cdb0ea
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-29 00:14:39 +1000

    url for home and productDetail page created

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-
+app_name = 'shop' 
 urlpatterns = [
-    path('',views.shop, name='shop'),
-    path('productDetail/<slug:slug>', views.productDetail, name='productDetail')
+    path('',views.home, name='home'),
+    path('productDetail/<slug:slug>', views.productDetail, name='productDetail'),
+    # path('addToCart/<int:pk',views.addToCart, name='addToCart')
+
 ]
