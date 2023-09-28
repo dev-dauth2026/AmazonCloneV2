94000796bc5f9b7dea7bfa9b804fbf1b7f0028f5
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-29 00:11:04 +1000

    created modal.js file

---
+++ static/js/navbar/modal.js
@@ -0,0 +1,9 @@
+// document.getElementById("openModal").addEventListener("mouseover", function() {
+//     var modal = document.getElementById("myModal");
+//     modal.classList.remove("hidden");
+// });
+
+// document.querySelector(".close").addEventListener("click", function() {
+//     var modal = document.getElementById("myModal");
+//     modal.classList.add("hidden");
+// });
