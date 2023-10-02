563a5129a5c89a1b8867926d7ea2164cf9c9d63e
Dauth Gurung <dauth22grg22@gmail.com>
2023-10-02 21:10:30 +1000

    register User and Customer model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
-# from django.contrib import admin
-# from .models import CostumerUser
+from django.contrib import admin
+from .models import Customer,Admin
 
-# # Register your models here.
-# admin.site.register(CostumerUser)
+# Register your models here.
+admin.site.register(Admin)
+admin.site.register(Customer)
