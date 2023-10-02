d77e63b8ddc9df729f888056df2be23764c2d7eb
Dauth Gurung <dauth22grg22@gmail.com>
2023-10-02 21:09:43 +1000

    Created Registration,Login, PasswordForgot and Password Reset Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,76 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Customer
+from django.forms import widgets
 
 
-class SignupForm(UserCreationForm):
+class CustomerRegistrationForm(forms.ModelForm):
+    username= forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password= forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    email= forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model=Customer
+        fields=["username", "password", "email", "full_name","address","mobile"]
+        widgets= {
+            'full_name':forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Full Name..'}),
+            'address':forms.TextInput(attrs={'class':'form-control','placeholder':'Enter address..'}),
+            'mobile':forms.TextInput(attrs={'class':'form-control','placeholder':'Enter mobile number..'}),
+        }
+
+    def clean_username(self):
+        uname=self.cleaned_data.get("username")
+        if User.objects.filter(username=uname).exists():
+            raise forms.ValidationError("Customer with this username  already exists.")
+        return uname
+
+    def clean_email(self):
+        em=self.cleaned_data.get("email")
+        if User.objects.filter(email=em).exists():
+            raise forms.ValidationError("Customer with this email already exists.")
+        return em
     
-    first_name=forms.CharField(max_length=30, required=False,help_text="Optional")
-    last_name=forms.CharField(max_length=30, required=False,help_text="Optional")
-    email=forms.EmailField(max_length=254,required=True,)
-    password1=forms.CharField(widget=forms.PasswordInput(attrs={'type': 'password'}))
-    password2=forms.CharField(widget=forms.PasswordInput(attrs={'type': 'password'}))
-    class Meta: 
-        model=User
-        fields = ['username','first_name','last_name','email','password1','password2']
 
 
-# class LoginForm(forms.Form):
-#     username= forms.CharField()
-#     password =forms.CharField(widget=forms.PasswordInput)
+class CustomerLoginForm(forms.Form):
+    username= forms.CharField(widget=widgets.TextInput(attrs={'class':'form-control','placeholder':'Enter the username...'}))
+    password= forms.CharField(widget=widgets.PasswordInput(attrs={'class':'form-control','placeholder':'Enter the password...'}))
+
+class PasswordForgotForm(forms.Form):
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        "class": "form-control",
+        "placeholder": "Enter the email used in customer account..."
+    }))
+
+    def clean_email(self):
+        e = self.cleaned_data.get("email")
+        if Customer.objects.filter(user__email=e).exists():
+            pass
+        else:
+            raise forms.ValidationError(
+                "Customer with this account does not exists..")
+        return e
+    
+
+class PasswordResetForm(forms.Form):
+    new_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'autocomplete': 'new-password',
+        'placeholder': 'Enter New Password',
+    }), label="New Password")
+    confirm_new_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'autocomplete': 'new-password',
+        'placeholder': 'Confirm New Password',
+    }), label="Confirm New Password")
+
+    def clean_confirm_new_password(self):
+        new_password = self.cleaned_data.get("new_password")
+        confirm_new_password = self.cleaned_data.get("confirm_new_password")
+        if new_password != confirm_new_password:
+            raise forms.ValidationError(
+                "New Passwords did not match!")
+        return confirm_new_password
+
+
